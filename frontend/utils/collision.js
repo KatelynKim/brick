3e4c5adccbbcf8045f4f9cb1d0501b7aca2de391
KatelynKim <wlsdud194@hotmail.com>
2024-03-26 01:27:33 -0700

    Update ball direction upon coliision with a bubble quadrant

--- frontend/utils/collision.js
+++ frontend/utils/collision.js
@@ -2,6 +2,7 @@ import Ball from '../classes/Ball.js'
 import Player from '../classes/Player.js'
 import { mapDefault } from '../maps.js'
 import { playerCanvas } from './canvas.js'
+import { MAX_BALL_ANGLE } from './constants.js'
 
 /**
  *
@@ -17,7 +18,7 @@ function handleCollisions(ball, player) {
     ball.x >= player.x &&
     ball.x <= player.x + player.length
   ) {
-    const anglePerUnitDistance = 45 / (player.length / 2)
+    const anglePerUnitDistance = MAX_BALL_ANGLE / (player.length / 2)
     const ballDistanceFromBarMidpoint = ball.x - (player.x + player.length / 2)
     const angle = 90 - anglePerUnitDistance * ballDistanceFromBarMidpoint
     ball.dx = ball.dy / Math.tan((Math.PI * angle) / 180)
@@ -45,9 +46,50 @@ function handleCollisions(ball, player) {
     )
     if (bubble.hp > 0 && distance <= bubble.radius + ball.radius) {
       bubble.setHp(0)
-      ball.dx *= -1
+      const quadrant = getQuadrant(ball, bubble)
+      updateBallDirection(quadrant, ball)
     }
     bubble.draw()
+  }
+}
+
+/**
+ * Updates the direction of the ball based on the quadrant on which it has collided with the bubble.
+ *
+ * @param {'UpperLeft' | 'UpperRight' | 'LowerLeft' | 'LowerRight'} quadrant - The ball quadrant in which the collision has occurred
+ */
+function updateBallDirection(quadrant, ball) {
+  const isBallMovingDownward = ball.dy > 0
+  const isBallMovingRight = ball.dx > 0
+
+  switch (quadrant) {
+    case 'UpperLeft':
+      if (isBallMovingRight) ball.dx *= -1
+      if (isBallMovingDownward) ball.dy *= -1
+      break
+    case 'UpperRight':
+      if (!isBallMovingRight) ball.dx *= -1
+      if (isBallMovingDownward) ball.dy *= -1
+      break
+    case 'LowerLeft':
+      if (isBallMovingRight) ball.dx *= -1
+      if (!isBallMovingDownward) ball.dy *= -1
+      break
+    case 'LowerRight':
+      if (!isBallMovingRight) ball.dx *= -1
+      if (!isBallMovingDownward) ball.dy *= -1
+      break
+    default:
+      return
+  }
+
+}
+
+function getQuadrant(ball, bubble) {
+  if (ball.y <= bubble.centerY) {
+    return ball.x <= bubble.centerX ? 'UpperLeft' : 'UpperRight'
+  } else {
+    return ball.x <= bubble.centerY ? 'LowerLeft' : 'LowerRight'
   }
 }
 
